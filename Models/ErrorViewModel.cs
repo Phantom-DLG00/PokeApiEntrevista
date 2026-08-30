dd62e4bb97167f260046add00de7aa9b3f337ec3
Diego Lara <diego.laraga18@gmail.com>
2026-08-30 12:52:42 -0600

    Proyecto Inicial MVC en .NET 8

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PokeApiEntrevista.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
